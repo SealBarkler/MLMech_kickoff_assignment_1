26ea6c6bc4be8c4ea524e38b08a06d4c0b19917d
Lars Kälber <ac144316@uni-stuttgart.de>
2025-01-13 11:22:52 +0100

    initial commit with README.md and the actual assignment(_1.py)

---
+++ assignment_1.py
@@ -0,0 +1,129 @@
+# %%
+def get_equal(a, b):
+    '''
+    returns a bool for the equality of a and b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_not_equal(a, b):
+    '''
+    returns a bool for the non-equality of a and b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_in(a, b):
+    '''
+    returns a bool for a being in b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_not_in(a, b):
+    '''
+    returns a bool for a not being in b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_smaller(a, b):
+    '''
+    returns a bool for a being smaller than b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_smaller_equal(a, b):
+    '''
+    returns a bool for a being smaller than or equal to b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_bigger(a, b):
+    '''
+    returns a bool for a being bigger than b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_bigger_equal(a, b):
+    '''
+    returns a bool for a being bigger than or equal to b
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_or(statement1, statement2):
+    '''
+    returns a bool for statement1 or statement2 being True
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_and(statement1, statement2):
+    '''
+    returns a bool for statement1 and statement2 being True
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_logical_or(statement1, statement2):
+    '''
+    returns a bool for statement1 or statement2 being True using the logical or
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_logical_and(statement1, statement2):
+    '''
+    returns a bool for statement1 and statement2 being True using the logical and
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+def get_xor(statement1, statement2):
+    '''
+    returns a bool for statement1 xor statement being True
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
